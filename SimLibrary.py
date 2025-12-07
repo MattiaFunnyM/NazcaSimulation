c4488b2d542c035e65b1e0f164c1f6cada3b683e
MattiaFunnyM <mattiacontithomas@gmail.com>
2025-12-07 18:54:20 +0100

    SimLibrary - finding_mode_from_geometry update for allowing different mode calculation

--- SimLibrary.py
+++ SimLibrary.py
@@ -41,10 +41,10 @@ def compute_geometry_bounds(geometry_list):
 
     return bounding_size, bounding_center
 
-def finding_mode_from_geometry(geometry, frequency=1, resolution=20, time=50):
+def finding_mode_from_geometry(geometry, mode=1, frequency=1, resolution=20, time=50):
     """
     This function generates the Ez and Hy field cross sections 
-    for the fundamental mode of the geometry in input.
+    for the TE modes of specified mode order of the geometry in input.
     The mode is calculates at the specified frequency.
     The precision of the simulation is determined by the resolution parameter.
     The simulations runs for the specified time.
@@ -64,7 +64,7 @@ def finding_mode_from_geometry(geometry, frequency=1, resolution=20, time=50):
         center=mp.Vector3(src_position, 0),
         size=mp.Vector3(0, sim_size.y),
         direction=mp.X,
-        eig_band=1
+        eig_band=mode
     )]
 
     # Create the meep simulation
@@ -73,8 +73,7 @@ def finding_mode_from_geometry(geometry, frequency=1, resolution=20, time=50):
         sources=source,
         geometry=geometry,
         resolution=resolution,
-        boundary_layers=pml_layers
-    )
+        boundary_layers=pml_layers)
 
     # Change output directory
     sim.use_output_directory("Output")
@@ -95,17 +94,42 @@ def finding_mode_from_geometry(geometry, frequency=1, resolution=20, time=50):
     Hy1 = sim.get_dft_array(dft, mp.Hy, 0)
 
     # Extract the cross section at that maxima (used for cross section and mode generation
-    x_max_index = int(pml_thickness * resolution) + 3
+    flat_index = np.argmax(np.real(Ez1))
+    x_max_index, _ = np.unravel_index(flat_index, Ez1.shape)
     Ez1_cross = Ez1[x_max_index, :]
     Hy1_cross = Hy1[x_max_index, :]
     
     # Calculate the normalization from the fields optical power
     dy = sim_size.y / resolution  
+
+    # Calculate the optical power as half of the product of electric field and magnetic field
     Power_vector = -0.5 * np.real(Ez1_cross * np.conj(Hy1_cross))
+
+    # The power value of the cross section is the integral of the optical power vector
     Power_value = np.abs(np.sum(Power_vector) * dy)
+    
+    # To normalize the fields, we need to divide by the square root of the power.
+    # In reality, np.abs(Ez1_cross) is normalized, not np.real(Ez1_cross) that oscillates
+    # Byt later Ez1 and Hy1 are properly normalized
+    Ez1_cross_norm = np.real(Ez1_cross) / np.sqrt(Power_value)
+    Hy1_cross_norm = np.real(Hy1_cross) / np.sqrt(Power_value) 
 
-    Ez1_cross = np.abs(Ez1_cross) / np.sqrt(Power_value)
-    Hy1_cross = np.abs(Hy1_cross) / np.sqrt(Power_value)
+    # Normalization by ratio to pass from np.real to np.abs
+    Ez1_cross_norm *= np.max(np.abs(Ez1_cross)) / np.max(np.real(Ez1_cross))
+    Hy1_cross_norm *= np.max(np.abs(Hy1_cross)) / np.max(np.real(Hy1_cross))
+    
+    # Get the material profile distribtuion
+    eps_data = sim.get_array(
+    component=mp.Dielectric,
+    center=mp.Vector3(),           
+    size=sim_size)
 
+    # Get the cross section of the material profile
+    eps_cross = eps_data[x_max_index, :]
+
+    # Calculate refractive index
+    eps_eff_cross = Power_vector * eps_cross / Power_value
+    neff = np.sqrt(np.sum(eps_eff_cross) * dy)
+                   
     # Return the field cross section 
-    return Ez1_cross, Hy1_cross
+    return Ez1_cross_norm, Hy1_cross_norm, neff
